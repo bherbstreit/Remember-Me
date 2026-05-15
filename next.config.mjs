70442734f387cd378e557ae1c715b71887dac030
bherbstreit <bailey.herbstreit@gmail.com>
2026-05-15 05:26:15 -0400

    Static Repo

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '/Remember-Me'
 
 const nextConfig = {
   output: 'export',
